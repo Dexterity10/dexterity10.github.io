c4a52ae99b69ca166e3212d1351cb1d94b63fa0c
Dexterity10 <alex.goemans@gmail.com>
2024-05-06 09:43:38 -1000

    v.21
    added comments to script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 showChapter = 1;
 maxChapter = 2;
 
-function prev() {
+// "first" button takes you to chapter 1 regardless of where you are
+
+function prev() { //immediate previous
   showChapter = Math.max(showChapter - 1, 1);
   window.location.href = 'chapter' + showChapter + '.html';
 }
-function next() {
+function next() { // immediate next
   showpage = Math.min(showChapter + 1, maxChapter);
   window.location.href = 'chapter' + showChapter + '.html';
 }
-function recent() {
+function recent() { // most recent chapter
   window.location.href = 'chapter' + maxChapter + ".html";
 }
