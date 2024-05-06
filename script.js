3d687e6e3157e13592d3da94b1b5db6182acb5c9
Dexterity10 <alex.goemans@gmail.com>
2024-05-06 09:37:30 -1000

    v0.20
    changed the code. Seems to be working, but also isn't...

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-showpage = 0; // works on a base 4 system. 0 = 1-4, 1 = 5-8, etc.
-page = [
-  "1-4",
-  "5-8",
-  "9-12"
-];
-const last = page[show.length - 1];
+showChapter = 1;
+maxChapter = 2;
 
 function prev() {
-  showpage = Math.max(showpage - 1, 0);
-  window.location.href = page[showpage] + '.html';
+  showChapter = Math.max(showChapter - 1, 1);
+  window.location.href = 'chapter' + showChapter + '.html';
 }
 function next() {
-  showpage = Math.min(showpage + 1, show.length);
-  window.location.href = page[showpage] + '.html';
+  showpage = Math.min(showChapter + 1, maxChapter);
+  window.location.href = 'chapter' + showChapter + '.html';
 }
 function recent() {
-  window.location.href = last + ".html";
+  window.location.href = 'chapter' + maxChapter + ".html";
 }
