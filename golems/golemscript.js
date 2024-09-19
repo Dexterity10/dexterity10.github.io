fd3a2d4a9019916513b88e534bf7f678c2e04588
Dexterity10 <alex.goemans@gmail.com>
2024-09-19 19:50:39 -0400

    golem work

---
+++ golems/golemscript.js
@@ -0,0 +1,23 @@
+
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+const page3 = document.getElementById("page3");
+
+function selectPage(button) {
+console.log(button)
+    if (button == 1) {
+        page1.style.display = 'block';
+        page2.style.display = 'none';
+        page3.style.display = 'none';
+    }
+    if (button == 2) {
+        page1.style.display = 'none';
+        page2.style.display = 'block';
+        page3.style.display = 'none';
+    }
+    if (button == 3) {
+        page1.style.display = 'none';
+        page2.style.display = 'none';
+        page3.style.display = 'block';
+    }
+}
